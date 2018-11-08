0c0b9cbd6614384be0dfd51a5c333d94c73fd96a
MzMing <459415188@qq.com>
2018-11-08 17:37:05 +0800

    2018-11-8 day01

---
+++ ly-registry/src/main/java/com/leyou/LyRegistry.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * < 描述 >
+ * <  >
+ *
+ * @author Aki
+ * @create 2018-11-08 9:57
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LyRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(LyRegistry.class,args);
+    }
+}
