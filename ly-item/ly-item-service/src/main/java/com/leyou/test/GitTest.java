0c64dcb99af78362850c662759141e6f0429ef3a
MzMing <459415188@qq.com>
2018-11-09 10:56:28 +0800

    2018-11-9 Test

--- ly-item/ly-item-service/src/main/java/com/leyou/test/GitTest.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/test/GitTest.java
@@ -1,5 +1,12 @@
 package com.leyou.test;
 
+/**
+ * < 描述 >
+ * <  >
+ *
+ * @author Aki
+ * @create 2018-11-09 10:27
+ */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
