2acc1b94cbdc786d952bd6e25f32f9484e806d40
MzMing <459415188@qq.com>
2018-11-13 17:16:52 +0800

    2018-11-13 显示品牌

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,16 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * < 描述 >
+ * <  >
+ *
+ * @author Aki
+ * @create 2018-11-13 15:53
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface BrandMapper extends Mapper<Brand> {
+
+}
