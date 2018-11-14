83dda8b139b7812a2fd6a15fc6b61105caf72ff2
MzMing <459415188@qq.com>
2018-11-14 16:56:11 +0800

    2018-11-14 新增品牌

---
+++ ly-upload/src/main/java/com/leyou/LyUploadService.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * < 描述 >
+ * <  >
+ *
+ * @author Aki
+ * @create 2018-11-14 16:48
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyUploadService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyUploadService.class, args);
+    }
+}
