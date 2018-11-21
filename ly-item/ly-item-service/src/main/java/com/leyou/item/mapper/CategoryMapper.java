33bfda079a26f59389ccaec5397b1f005aefcc94
MzMing <459415188@qq.com>
2018-11-21 14:11:32 +0800

    2018-11-21 商品查询（未实现新增）

--- ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -1,7 +1,11 @@
 package com.leyou.item.mapper;
 
 import com.leyou.item.pojo.Category;
+import tk.mybatis.mapper.additional.idlist.SelectByIdListMapper;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * < 接口的描述 >
@@ -11,5 +15,7 @@ import tk.mybatis.mapper.common.Mapper;
  * @create 2018-11-13 9:53
  */
 @org.apache.ibatis.annotations.Mapper
-public interface CategoryMapper extends Mapper<Category> {
+public interface CategoryMapper extends Mapper<Category> ,SelectByIdListMapper<Category, Long> {
+
+
 }
--- ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -5,7 +5,9 @@ import com.leyou.item.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * < 描述 >
@@ -24,5 +26,15 @@ public class CategoryService {
         Category t = new Category();
         t.setParentId(pid);
         return this.categoryMapper.select(t);
+    }
+
+    public List<String> queryNamesByIds(List<Long> ids) {
+        List<Category> list = this.categoryMapper.selectByIdList(ids);
+        List<String> names = new ArrayList<>();
+        for (Category category : list) {
+            names.add(category.getName());
+        }
+        return names;
+        // return list.stream().map(category -> category.getName()).collect(Collectors.toList());
     }
 }
